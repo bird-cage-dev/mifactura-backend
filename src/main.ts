1d6be53c4aa18d0ae27fe9e467fa19bbdecb313a
maikol415 <72170194+maikol415@users.noreply.github.com>
2025-05-28 14:00:44 -0500

    feat:invoice creation

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,39 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
   app.setGlobalPrefix('/v1/api');
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // dominio frontend
+    credentials: true,               // necesario para cookies
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: false,
       transform: true,
       always: true,
-      forbidNonWhitelisted: true,//
-      forbidUnknownValues: true
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
     }),
-  )
+  );
+
   const config = new DocumentBuilder()
     .setTitle('Swagger MiFactura')
     .setDescription('The MiFactura API description')
     .setVersion('1.0')
     .build();
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 main();
